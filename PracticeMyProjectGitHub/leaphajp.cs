965f0599985ba13487b4b5706d024f36623b2bdf
Jackson Leaphart <leaphajp@mail.uc.edu>
2024-04-02 12:00:01 -0400

    Created a new class that writes "I am Jackson".

---
+++ PracticeMyProjectGitHub/leaphajp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeMyProjectGitHub
+{
+    internal class leaphajp
+    {
+        public leaphajp() 
+        {
+            Console.WriteLine("I am Jackson");
+        }
+    }
+}
